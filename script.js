55e2c962e3384871b5d04beab378a7975cdea751
shafanajasim <jasimbrothers@gmail.com>
2024-02-18 17:51:14 +0000

    added script file

---
+++ script.js
@@ -0,0 +1,4 @@
+let quotes = ['Everything i do is for the pleasure of Allah','I talk to Allah,I pray to him','Allah is my only hope','We are nearer to him than his jugular vein','With Allah all things are possible'];
+let randomMsg = Math.floor(Math.random()*quotes.length);
+
+console.log('Your random quote is:'+ quotes[random]);
